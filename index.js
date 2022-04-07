7354eddb54811a616380080b1ad2720fbc4d8892
Ants-Kattel <ants.kattel@tptlive.ee>
2022-04-07 12:56:18 +0300

    Get a list of movies

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const app = require('express')()
 const port = 8080
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./docs/swagger.json')
+
+app.get('/movies', (req, res) => {
+    res.send(["Shawshank redemtion", "The Batman"])
+})
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)) 
+
 app.listen(port, () =>{
     console.log(`API up at: http://localhost:${port}`)
 })
